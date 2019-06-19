f23371f6df9b7145af6ab96d3a139d39651408d1
neverdieone <altiore@mail.ru>
2019-06-19 12:59:53 +0300

    Add function to get post statistic.

--- main.py
+++ main.py
@@ -2,6 +2,23 @@ from instabot import Bot
 import os
 from dotenv import load_dotenv
 import datetime
+import collections
+
+
+def get_post_statistic(post_id, days=90) -> collections.Counter:
+    counter = collections.Counter()
+    comments = bot.get_media_comments_all(post_id)
+    today = datetime.datetime.now()
+    for comment in comments:
+        user_id = comment['user_id']
+        comment_text = comment['text']
+        comment_date = datetime.datetime.fromtimestamp(comment['created_at'])
+        time_delta = today - comment_date
+        if time_delta.days <= days:
+            counter[user_id] += 1
+
+    return counter
+
 
 if __name__ == '__main__':
     load_dotenv()
@@ -10,12 +27,6 @@ if __name__ == '__main__':
     bot.login(username=os.getenv('INST_LOGIN'), password=os.getenv('INST_PASSWORD'))
 
     user_id = bot.get_user_id_from_username('cocacolarus')
-    user_medias = bot.get_total_user_medias(user_id)
+    user_medias = bot.get_total_user_medias(user_id)  # ID всех постов
 
-    for media_id in user_medias:
-        comments = bot.get_media_comments_all(media_id)
-        for comment in comments:
-            print(comment['text'])
-            comment_date = datetime.datetime.fromtimestamp(comment['created_at'])
-            print(comment_date)
-            print()
+    posts_statistic = sum([get_post_statistic(post_id) for post_id in user_medias])
